8e77824b5f9cef165d156e382d7032fd68fdf997
Zakariye <Zakariyeahmed0@gmail.com>
2023-01-23 16:36:15 -0800

    added basic win or lose logics for the game

--- js/main.js
+++ js/main.js
@@ -45,57 +45,44 @@
 		}
 		checkGameStatus();
 	}
-	}
+}
+
 // Function to check the game status
 	function checkGameStatus() {
+	if (checkWin()) {
+	   alert('You wone!');
+	   gameOver = true;
+	} else if (checkLose()) {
+	   alert('You lost. The word was ' + word);
+	   gameOver = true;
+	} else {
+	   alert('Guess a letter. You have ' + remainingGuesses + ' remaining guesses.');
+	}
+}
 
+// Function to check if the player has won
+	function checkWin() {
+		for (let i = 0; i < word.length; i++) {
+			if(!guessedLetters.includes(word[i])) {
+				return false;
+			}
+		}
+		return true;
 	}
 
+// Function to check if the player has lost
+	function checkLose() {
+		if (remainingGuesses === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+// Start the game
+selectWord();
 
 
-
-
-
-
-
-// Space-Man:
-
-// 1. Create an array of words for the game
-
-// 2. Select a random word from the array
-
-// 3. Create an empty array to store the guessed letters
-
-// 4. Create a variable to store the number of remaining guesses
-
-// 5. Create a variable to store whether the game is over
-
-// 6. Function to check if the player has won
-
-  // 6.5 Check if all letters in the word have been guessed
-
-
-// 7. Function to check if the player has lost
-
-
-// 8. Function to update the game status
-
-
-// 9. Function to handle player's guess
-
-
-// 10. Function to display the current word with unguessed letters replaced with underscores
-
-
-// 11. Start the game
-
-// 12. Get player's guess
-
-
-// 13. Continue getting player's guess until game is over
-
-
-// 14. The game is over and the player can no longer input guesses.
 
 
 
